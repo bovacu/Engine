57c00a79a72e0b26ecd0303f40909ff04bc450a2
vazquezborja@hotmail.es <cuestablanco1>
2020-07-29 20:49:53 +0200

    - Solved ENGINE_BIND_EVENT_FN warning finally.

--- engine/main/Core.h
+++ engine/main/Core.h
@@ -75,4 +75,4 @@
 #endif
 
 #define BIT(x) (1 << x)
-#define ENGINE_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+#define ENGINE_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
